71f3bdfa1bc93bfb179f1c4d2037cae84f1561dd
karen daniela <ckarendaniela133@gmail.com>
2026-08-19 12:29:33 -0500

    Ejercicio_2

---
+++ untitled/src/Ejercicio_2.java
@@ -0,0 +1,11 @@
+public class Ejercicio_2 {
+
+        public static void main(String[] args) {
+            saludarEstudiante("Karen", 18);
+        }
+
+        public static void saludarEstudiante(String nombre, int edad) {
+            System.out.println("Hola " + nombre + ", tienes " + edad + " años.");
+        }
+
+    }
