06080f5d72fdb63c3afbf114f6c18490093f29d1
karen daniela <ckarendaniela133@gmail.com>
2026-08-19 13:50:56 -0500

    Ejercicio_3

---
+++ untitled/src/Ejercicio_3.java
@@ -0,0 +1,12 @@
+public class Ejercicio_3 {
+
+    public static void main(String[] args) {
+        double area = calcularAreaRectangulo(5, 3);
+        System.out.println("El area del rectangulo es: " + area);
+    }
+
+    public static double calcularAreaRectangulo(double base, double altura) {
+        return base * altura;
+    }
+
+}
