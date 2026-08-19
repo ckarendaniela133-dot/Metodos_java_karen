79df9468e66504d22073f895ccfb965dd0aafc51
karen daniela <ckarendaniela133@gmail.com>
2026-08-19 11:53:44 -0500

    Ejercicio_1

---
+++ untitled/src/Ejercicio_1.java
@@ -0,0 +1,12 @@
+public class Ejercicio_1 {
+    public class Main {
+
+        public static void main(String[] args) {
+            mostrarBienvenida();
+        }
+
+        public static void mostrarBienvenida() {
+            System.out.println("Bienvenido al instituto");
+        }
+    }
+}
