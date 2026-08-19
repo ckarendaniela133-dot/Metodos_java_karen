c4fa17f75e41fb69ec3196490ac159f34162ad21
karen daniela <ckarendaniela133@gmail.com>
2026-08-19 14:12:12 -0500

    Ejercicio_4

---
+++ untitled/src/Ejercicio_4.java
@@ -0,0 +1,16 @@
+public class Ejercicio_4 {
+
+    public static void main(String[] args) {
+        boolean resultado = esPar(8);
+        System.out.println("El numero es par: " + resultado);
+    }
+
+    public static boolean esPar(int numero) {
+        if (numero % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
